0881a4adf15b5512e2c88dd6cd0e4ff14d14e41a
François Doussin <doussin.f@gmail.com>
2022-06-14 11:20:39 +0200

    clean script, add full helper and warning

--- app.py
+++ app.py
@@ -3,26 +3,51 @@ from bs4 import BeautifulSoup
 
 website = None;
 element = None;
+help = """
+📜 Web Scrapper Help :
+===================
+
+Flags :
+-------
+
+-h | --help       Help  
+-w | --website    Website url without protocol
+-e | --element    Element to be recovered
+"""
+warning = """
+⚠️  Warning
+==========
+
+This website doesn\'t exist !
+"""
+
 try:
-  opts, args = getopt.getopt(sys.argv[1:],'hw:e:',['website=','element='])
+    opts, args = getopt.getopt(
+        sys.argv[1:],
+        'hw:e:',
+        ['website=', 'element=', 'help']
+    )
 except getopt.GetoptError:
-  print('app.py -w <website> -e <element>')
-  sys.exit(2)
+    print(help)
+    sys.exit(2)
 
 for opt, arg in opts:
-  if opt == '-h':
-     print('app.py -w <website> -e <element>')
-     sys.exit()
-  elif opt in ('-w', '--website'):
-     website = arg
-  elif opt in ('-e', '--element'):
-     element = arg
+    if opt in ('-h', '--help'):
+        print(help)
+        sys.exit()
+    elif opt in ('-w', '--website'):
+        website = arg
+    elif opt in ('-e', '--element'):
+        element = arg
 
 if website != None and element != None:
-	requests = requests.get('https://' + website)
-	page = requests.content
-	soup = BeautifulSoup(page, 'html.parser')
-	for target in soup.find_all(element):
-	    print(target)
+    try:
+        requests = requests.get('https://' + website)
+        page = requests.content
+        soup = BeautifulSoup(page, 'html.parser')
+        for target in soup.find_all(element):
+            print(target)
+    except Exception:
+        print(warning)
 else:
-	print('app.py -w <website> -e <element>')
+    print(help)
